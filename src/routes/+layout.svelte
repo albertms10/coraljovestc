e0afe4612892e161efdf566fcdede2d3f0770c86
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-12-20 16:00:37 +0100

    style: 🎨 reformat code using spaces instead of tabs (#3)
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import { i18n } from '$lib/i18n';
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
-	import '../app.css';
+  import { i18n } from '$lib/i18n';
+  import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+  import '../app.css';
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
-	{@render children()}
+  {@render children()}
 </ParaglideJS>
