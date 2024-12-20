0668677ef1fc4f799f0d940422ffc4468a084800
Albert Mañosa <26429103+albertms10@users.noreply.github.com>
2024-12-20 15:59:57 +0100

    style: 🎨 reformat code using spaces instead of tabs
    Signed-off-by: Albert Mañosa <26429103+albertms10@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { page } from '$app/state';
-	import { i18n } from '$lib/i18n';
-	import * as m from '$lib/paraglide/messages.js';
-	import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
+  import { goto } from '$app/navigation';
+  import { page } from '$app/state';
+  import { i18n } from '$lib/i18n';
+  import * as m from '$lib/paraglide/messages.js';
+  import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
 
-	function switchToLanguage(newLanguage: AvailableLanguageTag) {
-		const canonicalPath = i18n.route(page.url.pathname);
-		const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
-		goto(localisedPath);
-	}
+  function switchToLanguage(newLanguage: AvailableLanguageTag) {
+    const canonicalPath = i18n.route(page.url.pathname);
+    const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
+    goto(localisedPath);
+  }
 </script>
 
 <h1>Coral Jove del Conservatori de Sant Cugat</h1>
 <h2>{m.welcome()}</h2>
 <footer>
-	<button onclick={() => switchToLanguage('en')}>English</button>
-	<button onclick={() => switchToLanguage('es')}>Español</button>
-	<button onclick={() => switchToLanguage('ca')}>Català</button>
+  <button onclick={() => switchToLanguage('en')}>English</button>
+  <button onclick={() => switchToLanguage('es')}>Español</button>
+  <button onclick={() => switchToLanguage('ca')}>Català</button>
 </footer>
